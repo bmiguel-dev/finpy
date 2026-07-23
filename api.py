9546f0e7f9b7e98c464927595e52cadebc216553
bmiguel-dev <bnorodrigues07@gmail.com>
2026-07-23 01:31:53 -0300

    retira a conexão do método construtor da class Financeiro (atualmente no service), lifespan com criação das tabelas do banco de dados, Depends nas rotas com conexão do banco

--- api.py
+++ api.py
@@ -2,15 +2,19 @@ from fastapi import FastAPI, HTTPException, Request, Depends,Query
 from fastapi.responses import JSONResponse
 from services import Financeiro
 from models.transacao import CriarTransacoes, CorrigirTransacoes, FiltrarTransacoes, ResponseTransacoes,ResponseMetricas,CategoriaTotal,Metricas
-from database import conect_db
-from typing import Optional
 import sqlite3
 from pydantic import ValidationError
+from contextlib import asynccontextmanager
+financeiro = Financeiro ()
 
-app = FastAPI()
+print("DEBUG: aplication working! S2")
+@asynccontextmanager
+async def lifespan (app : FastAPI):
+    print("DEBUG: lifespan working! S2")
+    financeiro.iniciate_table()
+    yield print("DEBUG: API RODANDO COM LIFESPAN")
 
-financeiro = Financeiro (conexão_banco= conect_db)
-financeiro.iniciate_table()
+app = FastAPI(lifespan=lifespan)
 
 @app.exception_handler(sqlite3.Error)
 def erro_banco (requisicao : Request, erro : sqlite3.Error ):
@@ -34,44 +38,44 @@ def home ():
 
 @app.get("/transacoes")
 def listar_transacoes (categorias : list[int] = Query(default=None,title="Categorias ID", alias="cat"),
-                       filtro : FiltrarTransacoes = Depends(FiltrarTransacoes)):
-    dados = financeiro.search_by_filter(categorias=categorias,filtro=filtro)
+                       filtro : FiltrarTransacoes = Depends(FiltrarTransacoes), conn : sqlite3.Connection = Depends(financeiro.conect_db)):
+    dados = financeiro.search_by_filter(categorias=categorias,filtro=filtro,conn=conn)
     return [ResponseTransacoes(**dict(d)) for d in dados ]
     
 @app.get("/transacoes/metricas", status_code=200,response_model= ResponseMetricas)
-def exibir_metricas():
-    lista_categorias = [CategoriaTotal(**dict(d)) for d in financeiro.all_cat_values()]
-    metrica = Metricas(**dict(financeiro.get_balance_and_expense()))
+def exibir_metricas(conn : sqlite3.Connection = Depends(financeiro.conect_db)):
+    lista_categorias = [CategoriaTotal(**dict(d)) for d in financeiro.all_cat_values(conn=conn)]
+    metrica = Metricas(**dict(financeiro.get_balance_and_expense(conn=conn)))
     return  ResponseMetricas(categoria_total=lista_categorias,metricas_= metrica)
 
 @app.get("/transacoes/{id_}",  response_model=ResponseTransacoes)
-def transacao_por_id (id_: int):   
-    dados = financeiro.search_by_id(id_=id_)
+def transacao_por_id (id_: int, conn : sqlite3.Connection = Depends(financeiro.conect_db)):   
+    dados = financeiro.search_by_id(id_=id_, conn=conn)
     if not dados:
         raise HTTPException(status_code=404, detail= "Transação não encontrada.")
     return dict(dados)
 
 @app.post("/transacoes/new",status_code=201,response_model=ResponseTransacoes)
-def criar_transacao (transacoes: CriarTransacoes):
-    transacao_adicionada  = financeiro.adict_transaction(transacoes)
-    response_transacao = financeiro.search_by_id(transacao_adicionada)
+def criar_transacao (transacoes: CriarTransacoes, conn : sqlite3.Connection = Depends(financeiro.conect_db)):
+    transacao_adicionada  = financeiro.adict_transaction(transacoes,conn)
+    response_transacao = financeiro.search_by_id(transacao_adicionada,conn)
     return dict(response_transacao)
 
 @app.delete("/transacoes/{id_}", status_code = 204)
-def deletar_transacoes (id_:int):
-        id_confirmado = financeiro.search_by_id(id_=id_)
+def deletar_transacoes (id_:int , conn : sqlite3.Connection = Depends(financeiro.conect_db)):
+        id_confirmado = financeiro.search_by_id(id_=id_, conn=conn)
         if not id_confirmado:
             raise HTTPException(status_code=404, detail= "Transação não encontrada.")
-        financeiro.remove_transaction(id_)
+        financeiro.remove_transaction(id_,conn)
         return 
     
 @app.patch("/transacoes/{id_}", status_code= 200, response_model= ResponseTransacoes)
-def corrigir_transacao (id_:int, dados: CorrigirTransacoes):
-        id_confirmado = financeiro.search_by_id(id_=id_)
+def corrigir_transacao (id_:int, dados: CorrigirTransacoes , conn : sqlite3.Connection = Depends(financeiro.conect_db)):
+        id_confirmado = financeiro.search_by_id(id_=id_,conn=conn)
         if not id_confirmado:
             raise HTTPException(status_code=404, detail= "Transação não encontrada.")
-        financeiro.correct_transaction(id_=id_, dados=dados)
-        id_return = financeiro.search_by_id(id_=id_)
+        financeiro.correct_transaction(id_=id_, dados=dados,conn=conn)
+        id_return = financeiro.search_by_id(id_=id_,conn=conn)
         return dict(id_return)
       
 
--- services/financeiro.py
+++ services/financeiro.py
@@ -2,131 +2,131 @@ from typing import Callable
 import sqlite3
 from models import *
 
+
 class Financeiro:
    
-    def __init__(self,conexão_banco : Callable[[],sqlite3.Connection] ):
-        self.conectar_banco = conexão_banco
+    def __init__(self, db_name = 'finpy.db' ):
+        self.db_name = db_name
 
     def iniciate_table (self):
-        self.create_table_category()
-        self.create_table_transactions()
-        self.create_idx_category()
-        self.create_idx_date()
-        
-    def create_table_category(self):
-        with self.conectar_banco() as banco:
-            cursor = banco.cursor()
+        with sqlite3.connect(self.db_name) as conn:
+            self.create_table_category(conn=conn)
+            self.create_table_transactions(conn=conn)
+            self.create_idx_category(conn=conn)
+            self.create_idx_date(conn=conn)
+
+    def conect_db(self):
+        banco = sqlite3.connect('finpy.db')
+        banco.execute("PRAGMA foreign_keys = ON;")
+        banco.row_factory = sqlite3.Row
+        try:
+            yield banco
+        finally:
+            banco.close() 
+
+    def create_table_category(self,conn : sqlite3.Connection):
+            cursor = conn.cursor()
             cursor.execute(''' CREATE TABLE IF NOT EXISTS categorias (id INTEGER NOT NULL PRIMARY KEY, nome TEXT NOT NULL UNIQUE, tipo INTEGER NOT NULL)''')
             cursor.executemany('''INSERT OR IGNORE INTO categorias (id, nome, tipo) VALUES (?,?,?)''', Categoria.lista_categorias() )
-            banco.commit()
-
-    def create_table_transactions(self):
-        with self.conectar_banco() as banco:
-            cursor = banco.cursor() 
+            conn.commit()
+    
+    def create_table_transactions(self, conn : sqlite3.Connection):
+            cursor = conn.cursor() 
             cursor.execute(''' CREATE TABLE IF NOT EXISTS transacoes (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                                                       categoria_id INTEGER,
                                                                       valor REAL NOT NULL,
                                                                       descricao TEXT NOT NULL,
                                                                       data DATE NOT NULL,
                             FOREIGN KEY (categoria_id) REFERENCES categorias(id))''')
-            banco.commit() 
+            conn.commit() 
 
-    def create_idx_category (self):
-        with self.conectar_banco() as banco:
-            cursor = banco.cursor()
-            cursor.execute('''CREATE INDEX IF NOT EXISTS idx_transacoes_categoria_id ON transacoes(categoria_id)''')
-            banco.commit()
+    def create_idx_category (self, conn : sqlite3.Connection ):
+        cursor = conn.cursor()
+        cursor.execute('''CREATE INDEX IF NOT EXISTS idx_transacoes_categoria_id ON transacoes(categoria_id)''')
+        conn.commit()
         
-    def create_idx_date (self):
-        with self.conectar_banco() as banco:
-            cursor = banco.cursor()
-            cursor.execute('''CREATE INDEX IF NOT EXISTS idx_transacoes_data ON transacoes(data)''')
-            banco.commit()
+    def create_idx_date (self, conn : sqlite3.Connection ):
+        cursor = conn.cursor()
+        cursor.execute('''CREATE INDEX IF NOT EXISTS idx_transacoes_data ON transacoes(data)''')
+        conn.commit()
 
-    def adict_transaction (self, entrada_dado : CriarTransacoes ):
-        with self.conectar_banco() as banco:
-            cursor = banco.cursor()
-            cursor.execute('''INSERT INTO transacoes (categoria_id, valor, descricao, data)
-                            VALUES (:categoria_id,:valor,:descricao,:data) ''', entrada_dado.model_dump())
-            banco.commit()
-            return cursor.lastrowid
+    def adict_transaction (self, entrada_dado : CriarTransacoes, conn : sqlite3.Connection ):
+        cursor = conn.cursor()
+        cursor.execute('''INSERT INTO transacoes (categoria_id, valor, descricao, data)
+                        VALUES (:categoria_id,:valor,:descricao,:data) ''', entrada_dado.model_dump())
+        conn.commit()
+        return cursor.lastrowid
             
-    def remove_transaction (self, id:int):
-        with self.conectar_banco() as banco:
-            cursor = banco.cursor()
-            cursor.execute('''DELETE FROM transacoes WHERE id = ?''', [id])
-            banco.commit()
+    def remove_transaction (self, id:int , conn : sqlite3.Connection):
+        cursor = conn.cursor()
+        cursor.execute('''DELETE FROM transacoes WHERE id = ?''', [id])
+        conn.commit()
 
-    def search_by_filter (self,categorias:list[int], filtro : FiltrarTransacoes):
-        with self.conectar_banco() as banco:
-            cursor = banco.cursor()
-            dados = filtro.model_dump()
-            print("DEBUG filtro recebido:", dados)
-            data_i = dados.get('d_inicio')
-            data_f = dados.get('d_fim') 
-            query = '''SELECT transacoes.*, categorias.nome FROM transacoes
+    def search_by_filter (self,categorias:list[int], filtro : FiltrarTransacoes , conn : sqlite3.Connection):
+        cursor = conn.cursor()
+        dados = filtro.model_dump()
+        print("DEBUG filtro recebido:", dados)
+        data_i = dados.get('d_inicio')
+        data_f = dados.get('d_fim') 
+        query = '''SELECT transacoes.*, categorias.nome FROM transacoes
                            INNER JOIN categorias ON transacoes.categoria_id = categorias.id
                            WHERE 1=1'''
-            parametros = []
-            if categorias:
-                place_holders = ', '.join(['?'] * len(categorias))
-                query += f" AND categorias.id IN ({place_holders})"
-                parametros.extend(categorias)
-            if data_i and data_f:
-                query += f" AND transacoes.data BETWEEN ? AND ?"
-                parametros.extend([data_i, data_f])
-            cursor.execute(query,parametros)
-            dados_banco = cursor.fetchall()
-            return dados_banco
+        parametros = []
+        if categorias:
+            place_holders = ', '.join(['?'] * len(categorias))
+            query += f" AND categorias.id IN ({place_holders})"
+            parametros.extend(categorias)
+        if data_i and data_f:
+            query += f" AND transacoes.data BETWEEN ? AND ?"
+            parametros.extend([data_i, data_f])
+        cursor.execute(query,parametros)
+        dados_banco = cursor.fetchall()
+        return dados_banco
     
   
     
 
-    def search_by_id (self, id_: int):
-        with self.conectar_banco() as banco:
-            cursor = banco.cursor()
-            cursor.execute('''SELECT transacoes.*  FROM transacoes
+    def search_by_id (self, id_: int , conn : sqlite3.Connection):
+        cursor = conn.cursor()
+        cursor.execute('''SELECT transacoes.*  FROM transacoes
                            WHERE transacoes.id = ?''', [id_] )
-            dado = cursor.fetchone()
-            return dado
+        dado = cursor.fetchone()
+        return dado
     
     
-    def all_cat_values (self) -> list[sqlite3.Row]:
-        with self.conectar_banco() as banco:
-            cursor = banco.cursor() 
-            cursor.execute('''SELECT SUM(transacoes.valor) AS total_valores, categorias.nome AS nome_categoria
+    def all_cat_values (self , conn : sqlite3.Connection) -> list[sqlite3.Row]:
+        cursor = conn.cursor() 
+        cursor.execute('''SELECT SUM(transacoes.valor) AS total_valores, categorias.nome AS nome_categoria
                               FROM transacoes INNER JOIN categorias ON transacoes.categoria_id = categorias.id
                               GROUP BY categorias.nome''')
-            dados = cursor.fetchall()
-            return dados
+        dados = cursor.fetchall()
+        return dados
     
-    def get_balance_and_expense (self) -> sqlite3.Row | None:
-        with self.conectar_banco() as banco:  
-            cursor = banco.cursor()
-            cursor.execute('''SELECT COALESCE(SUM(CASE WHEN categorias.tipo = 1 THEN transacoes.valor ELSE 0 END),0) AS saldo_total,
+    def get_balance_and_expense (self , conn : sqlite3.Connection) -> sqlite3.Row | None:
+        cursor = conn.cursor()
+        cursor.execute('''SELECT COALESCE(SUM(CASE WHEN categorias.tipo = 1 THEN transacoes.valor ELSE 0 END),0) AS saldo_total,
                             COALESCE(SUM(CASE WHEN categorias.tipo = 2 THEN transacoes.valor ELSE 0 END),0) AS despesa_total, 
                            COALESCE(SUM(CASE WHEN categorias.tipo = 1 THEN transacoes.valor ELSE 0 END),0) - 
                             COALESCE(SUM(CASE WHEN categorias.tipo = 2 THEN transacoes.valor ELSE 0 END),0) AS total_liquido
                             FROM transacoes
                             INNER JOIN categorias ON transacoes.categoria_id = categorias.id
                            ''')
-            dados = cursor.fetchone()
-            return dados
+        dados = cursor.fetchone()
+        return dados
     
         
  
-    def correct_transaction (self, id_, dados : CorrigirTransacoes):
+    def correct_transaction (self, id_, dados : CorrigirTransacoes ,conn : sqlite3.Connection):
         dados_dict = {chave:valor for chave,valor in  dados.model_dump().items() if valor is not None}
         place_holder = ", ".join([f'{chave} = ?' for chave in  dados_dict.keys()])
         parametros = []
         parametros.extend(list(dados_dict.values()))
         parametros.append(id_)
         query = f"UPDATE transacoes SET {place_holder} WHERE transacoes.id = ?"
-        with self.conectar_banco() as banco: 
-            cursor = banco.cursor()
-            cursor.execute(query,parametros)
-            banco.commit()
-            return True
+        cursor = conn.cursor()
+        cursor.execute(query,parametros)
+        conn.commit()
+        return True
         
 
     
